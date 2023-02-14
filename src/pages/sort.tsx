36dc979b8a7550054a2e2d795361ab321be96b76
Sam Svindland <sam@Sams-MacBook-Pro.local>
2023-02-14 11:45:05 -0500

    fix link on sort page

--- src/pages/sort.tsx
+++ src/pages/sort.tsx
@@ -31,7 +31,7 @@ const Sort: NextPage = () => {
                     <Sorter />
                     <Link
                         className="my-2 rounded-full bg-white/10 px-10 py-3 text-center font-semibold text-white no-underline transition hover:bg-white/20"
-                        href="/Interview"
+                        href="/interview"
                     >
                         Interview
                     </Link>
